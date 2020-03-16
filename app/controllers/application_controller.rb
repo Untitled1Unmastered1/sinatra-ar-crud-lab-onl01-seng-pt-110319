8fd6e32a0a841601ecafd55c1d01fe47d20f396c
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-16 03:20:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,6 +8,52 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    
+    @article = Article.new 
+    
+    erb :new 
   end
+  
+  post '/articles' do 
+    
+    @article = Article.create(params)
+    
+    erb :show 
+  end 
+  
+  get '/articles' do 
+    
+    @articles= Article.all 
+    
+    erb :index 
+  end 
+  
+  get '/articles/:id' do 
+    
+    @article = Article.find(params[:id])
+    
+    erb:show 
+  end 
+  
+  get '/articles/:id/edit' do 
+    
+    @article = Article.find(params[:id])
+    
+    erb :edit 
+  end 
+  
+  patch '/articles/:id' do 
+    
+    @article = Article.update(params["title"], params["content"])
+    
+    erb :show 
+  end 
+  
+  delete '/articles/:id' do 
+    
+    @article = Article.destroy(params[:id])
+    
+    erb :delete 
+  end 
 end
